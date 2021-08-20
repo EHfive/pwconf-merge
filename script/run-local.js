abd6316c302c6902cec40179b9b189ae7382294a
Huang-Huang Bao <eh5@sokka.cn>
2021-08-20 21:17:45 +0800

    fix: minor fixes

--- script/run-local.js
+++ script/run-local.js
@@ -3,11 +3,13 @@ import { pwConfMerge } from '../lib/index.js'
 import { __basedir } from '../lib/constants.js'
 
 async function runLocal() {
+  console.time('merge')
   await pwConfMerge(
     path.join(__basedir, 'sample/pwconf-merge'),
-    path.join(__basedir, 'sample/pipewire-conf'),
-    path.join(__basedir, 'sample/pipewire-conf-data')
+    path.join(__basedir, 'sample/pipewire-conf-data'),
+    path.join(__basedir, 'sample/pipewire-conf')
   )
+  console.timeEnd('merge')
 }
 
 runLocal().catch((e) => {
